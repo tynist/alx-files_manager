20ea94aae00ab586c79930acd39fe4df35663a51
Peter Ughasi <ximonak@gmail.com>
2023-06-26 11:32:00 +0000

    files manager

--- utils/redis.js
+++ utils/redis.js
@@ -1,45 +1,36 @@
-// Import the redis and util modules
+// module contains class RedisClient Redis utils :-get, set, del methods
 const redis = require('redis');
 const { promisify } = require('util');
 
-// Create a class for the Redis client
 class RedisClient {
   constructor() {
-    // Create a Redis client
-    this._client = redis.createClient();
-
-    // Set up an error handler
-    this._client.on('error', (error) => {
-      console.error('Redis connection error:', error);
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.log('Redis client not connected to the server:', err);
     });
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.expireAsync = promisify(this.client.expire).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
-  // Check if the client is connected
   isAlive() {
-    return this._client.connected;
+    return this.client.connected;
   }
 
-  // Get a key asynchronously
-  getAsync = promisify((key, callback) => {
-    // Get the key from Redis
-    this._client.get(key, callback);
-  });
+  async get(key) {
+    const val = await this.getAsync(key);
+    return val;
+  }
 
-  // Set a key asynchronously
-  setAsync = promisify((key, value, duration, callback) => {
-    // Set the key in Redis
-    this._client.setex(key, duration, value, callback);
-  });
+  async set(key, val, dur) {
+    this.setAsync(key, val);
+    this.expireAsync(key, dur);
+  }
 
-  // Delete a key asynchronously
-  delAsync = promisify((key, callback) => {
-    // Delete the key from Redis
-    this._client.del(key, callback);
-  });
+  async del(key) {
+    this.delAsync(key);
+  }
 }
-
-// Create a new Redis client instance
 const redisClient = new RedisClient();
-
-// Export the Redis client instance
 module.exports = redisClient;
