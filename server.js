3ab7a0b150a6c5b72234290329d66b7b08db7ad9
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-26 14:56:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const routes = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Enable JSON, use the route module
-app.use(express.json());
-app.use('/', routes);
+// Use the router to set up routes for the server
+router(server);
 
 // Start the server and listen on the specified port
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server listening at https://localhost:${port}/`);
 });
