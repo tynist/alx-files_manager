18e4dc9d857b01a1e7283abf55808d49a26a013d
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-26 14:13:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+// Import Express module and the routes module
 const express = require('express');
-const controllerRouting = require('./routes/index');
+const routes = require('./routes/index');
 
+// Create an Express application and sets port number
 const app = express();
 const port = process.env.PORT || 5000;
 
-controllerRouting(app);
+// Enable JSON, use the route module
+app.use(express.json());
+app.use('/', routes);
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
-  console.log(`Express Server app running on http://localhost:${port}/`);
+  console.log(`Server listening on port ${port}`);
 });
-module.exports = app;
