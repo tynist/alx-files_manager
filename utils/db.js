41cbe4dee2f0b81867f656b26ffc693ff105651b
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-26 13:16:49 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,32 +1,45 @@
-// module contains class DBClient:-nbUsers, nbFiles, isAlive methods
 const { MongoClient } = require('mongodb');
 
 class DBClient {
   constructor() {
-    this.dbName = null;
+    // initializes the `databaseName` value to null
+    this.databaseName = null;
+
+    // Get MongoDB connection details from env variabless or use defaults
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || '27017';
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}/`;
+    
+    // connects to the MongoDB database
     MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
       if (error) throw (error);
-      this.dbName = client.db(database);
+
+      // Store the database ref in the databaseName variable
+      this.databaseName = client.db(database);
     });
   }
 
+  // Check if `databaseName` has a database reference
   isAlive() {
-    return !!this.dbName;
+    return !!this.databaseName;
   }
 
+  // Gets the number of documents in the `users` collection
   async nbUsers() {
-    const nbusers = await this.dbName.collection('users').countDocuments();
+    const nbusers = await this.databaseName.collection('users').countDocuments();
     return nbusers;
   }
 
+  // Gets the number of documents in the `files` collection
   async nbFiles() {
-    const nbfiles = await this.dbName.collection('files').countDocuments();
+    const nbfiles = await this.databaseName.collection('files').countDocuments();
     return nbfiles;
   }
 }
+
+// Create an instance of DBClient
 const dbClient = new DBClient();
+
+// Export the DBClient instance as the module's default export
 module.exports = dbClient;
