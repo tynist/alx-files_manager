5f734c8fdf7696262f4807aec6271c61d7bed3cf
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-26 13:25:21 +0100

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,13 +1,17 @@
+// Import the redis client and the db client
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
+// Controller class for the app
 class AppController {
+  // Get the status of Redis and MongoDB connections
   static getStatus(request, response) {
     return response.status(200).send(
       `{"redis": ${redisClient.isAlive()}, "db": ${dbClient.isAlive()}}`,
     );
   }
 
+  // Get the number of users and files from the MongoDB database
   static async getStats(request, response) {
     const nbusers = await dbClient.nbUsers();
     const nbfiles = await dbClient.nbFiles();
