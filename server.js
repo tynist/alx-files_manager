b4920688495a29b9f9855b8f592525db81ef05de
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-28 18:33:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-import express from 'express';
-import { env } from 'process';
+const express = require('express');
+const { env } = require('process');
 
 const mainRoute = require('./routes/index');
 
 const app = express();
 const port = env.PORT || 5000;
-app.use(express.json());
-app.use(mainRoute);
-app.listen(port, '127.0.0.1');
 
-export default app;
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Mount the main route
+app.use(mainRoute);
+
+// Start the server
+app.listen(port, '127.0.0.1');
