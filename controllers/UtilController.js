068727d25612769fd73b1c8f50d38b1825d513b5
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-28 17:23:28 +0100

    Update UtilController.js

--- controllers/UtilController.js
+++ controllers/UtilController.js
@@ -1,23 +1,28 @@
-import { createHash } from 'crypto';
-import { promises } from 'fs';
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+const { createHash } = require('crypto');
+const { promises } = require('fs');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
-export default class UtilController {
+class UtilController {
+  // Generates SHA1 hash for a given string.
   static SHA1(str) {
     return createHash('sha1').update(str).digest('hex');
   }
 
+  // Reads a file from the given path.
   static async readFile(path) {
     return promises.readFile(path, 'utf8');
   }
 
+  // Middleware function to handle authentication token.
   static async token(request, response, next) {
     let token = request.headers['x-token'];
-    token = 'auth_$(token)';
+    token = `auth_${token}`;
     const userId = await redisClient.get(token);
     const user = await dbClient.filterUser({ _id: userId });
+
     if (!user) {
+      // Unauthorized user
       response.status(401).json({ error: 'Unauthorized' }).end();
     } else {
       request.user = user;
@@ -26,3 +31,5 @@ export default class UtilController {
     }
   }
 }
+
+module.exports = UtilController;
