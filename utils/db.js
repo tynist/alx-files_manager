bad7290b51a8b66f5723a4c23e3f2b987a550fa9
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-28 17:01:03 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,43 +1,44 @@
-import { env } from 'process';
-import { MongoClient, ObjectId } from 'mongodb';
+const { env } = require('process');
+const { MongoClient, ObjectId } = require('mongodb');
 
-// eslint-disable-next-line import/prefer-default-export
-export class DBClient {
+class DBClient {
   constructor() {
+    // Set up the connection from env variable or use the default value
     const host = env.DB_PORT ? env.DB_PORT : '127.0.0.1';
     const port = env.DB_HOST ? env.DB_HOST : 27017;
     const database = env.DB_DATABASE ? env.DB_DATABASE : 'files_manager';
-    this.myClient = MongoClient(`mongodb://${host}:${port}/${database}`);
+    this.myClient = new MongoClient(`mongodb://${host}:${port}/${database}`);
     this.myClient.connect();
   }
 
+  // Check if the MongoDB client is connected to the server
   isAlive() {
     return this.myClient.isConnected();
   }
 
   async nbUsers() {
-    /* returns number of documents in the collection users */
+    // Returns the number of documents in the 'users' collection
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.countDocuments();
   }
 
   async nbFiles() {
-    /* returns number of documents in the collection files */
+    // Returns the number of documents in the 'files' collection
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('files');
     return myCollection.countDocuments();
   }
 
   async userExists(email) {
-    /* returns true if the user with the given email exists */
+    // Returns true if a user with the given email exists
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.findOne({ email });
   }
 
   async newUser(email, passwordHash) {
-    /* creates a new user with the given email and passwordHash */
+    // Creates a new user with the given email and passwordHash
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.insertOne({ email, passwordHash });
@@ -47,7 +48,7 @@ export class DBClient {
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     if ('_id' in filters) {
-      // eslint-disable-next-line no-param-reassign
+      // Convert the '_id' filter to ObjectId format
       filters._id = ObjectId(filters._id);
     }
     return myCollection.findOne(filters);
@@ -58,13 +59,15 @@ export class DBClient {
     const myCollection = myDB.collection('files');
     const idFilters = ['_id', 'userId', 'parentId'].filter((prop) => prop in filters && filters[prop] !== '0');
     idFilters.forEach((i) => {
-      // eslint-disable-next-line no-param-reassign
+      // Convert id filters to ObjectId format
       filters[i] = ObjectId(filters[i]);
     });
     return myCollection.findOne(filters);
   }
 }
 
+// Create an instance of the DBClient class
 const dbClient = new DBClient();
 
-export default dbClient;
+// Export the DBClient instance as a module
+module.exports = dbClient;
