dcfcb90f08eb0cdca98272ff72aa3d11f4d945cf
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-28 22:37:52 +0100

    AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,18 +2,22 @@ const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class AppController {
-  // Get the status of Redis and the database
-  static async getStatus(request, response) {
-    const redisStatus = await redisClient.isAlive();
-    const dbStatus = await dbClient.isAlive();
-    response.status(200).json({ redis: redisStatus, db: dbStatus }).end();
+  // Method to get the status of the Redis and database connections
+  static getStatus(request, response) {
+    // Check if Redis and database connections are alive
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  // Get statistics about the number of users and files in the database
+  // Method to get the number of users and files in the database
   static async getStats(request, response) {
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
-    response.status(200).json({ users, files }).end();
+    // Get the number of users from the database
+    const usersNum = await dbClient.nbUsers();
+
+    // Get the number of files from the database
+    const filesNum = await dbClient.nbFiles();
+
+    // Send the number of users and files as a JSON response
+    response.status(200).json({ users: usersNum, files: filesNum });
   }
 }
 
