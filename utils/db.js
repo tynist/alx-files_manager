0ee0b5c851b850c4f5085656cdd31b14b1a1dbc5
Peter Ughasi <ximonak@gmail.com>
2023-06-28 08:40:00 +0000

    files manaher

--- utils/db.js
+++ utils/db.js
@@ -1,88 +1,70 @@
-const { env } = require('process');
-const { MongoClient, ObjectId } = require('mongodb');
+import { env } from 'process';
+import { MongoClient, ObjectId } from 'mongodb';
 
-class DBClient {
+// eslint-disable-next-line import/prefer-default-export
+export class DBClient {
   constructor() {
-    // Get MongoDB connection details from env variabless or use defaults
     const host = env.DB_PORT ? env.DB_PORT : '127.0.0.1';
     const port = env.DB_HOST ? env.DB_HOST : 27017;
     const database = env.DB_DATABASE ? env.DB_DATABASE : 'files_manager';
-
-    // Initialize the MongoDB client and establish the connection
     this.myClient = MongoClient(`mongodb://${host}:${port}/${database}`);
     this.myClient.connect();
   }
 
-  // Check if the database client is alive and connected
   isAlive() {
     return this.myClient.isConnected();
   }
 
-  // Gets the number of documents in the 'users' collection
   async nbUsers() {
+    /* returns number of documents in the collection users */
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.countDocuments();
   }
 
-  // Get the number of documents in the 'files' collection
   async nbFiles() {
+    /* returns number of documents in the collection files */
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('files');
     return myCollection.countDocuments();
   }
 
-  // Check if a user with the given email exists
   async userExists(email) {
+    /* returns true if the user with the given email exists */
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.findOne({ email });
   }
 
-  // Create a new user with the given email and passwordHash
   async newUser(email, passwordHash) {
+    /* creates a new user with the given email and passwordHash */
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
     return myCollection.insertOne({ email, passwordHash });
   }
 
-  // Filter and find a user based on the provided filters
   async filterUser(filters) {
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('users');
-
-    const filtered = { ...filters }; // Shallow copy of filters object
-
-    if ('_id' in filtered) {
-      // Convert '_id' filter to ObjectId
-      filtered._id = ObjectId(filtered._id);
+    if ('_id' in filters) {
+      // eslint-disable-next-line no-param-reassign
+      filters._id = ObjectId(filters._id);
     }
-
-    return myCollection.findOne(filtered);
+    return myCollection.findOne(filters);
   }
 
-  // Filter and find files based on the provided filters
   async filterFiles(filters) {
     const myDB = this.myClient.db();
     const myCollection = myDB.collection('files');
-
-    const idFilters = ['_id', 'userId', 'parentId'].filter(
-      (prop) => prop in filters && filters[prop] !== '0',
-    );
-
-    const filtered = { ...filters }; // Shallow copy of filters object
-
+    const idFilters = ['_id', 'userId', 'parentId'].filter((prop) => prop in filters && filters[prop] !== '0');
     idFilters.forEach((i) => {
-      // Convert '_id', 'userId', and 'parentId' filters to ObjectId
-      filtered[i] = ObjectId(filtered[i]);
+      // eslint-disable-next-line no-param-reassign
+      filters[i] = ObjectId(filters[i]);
     });
-
-    return myCollection.findOne(filtered);
+    return myCollection.findOne(filters);
   }
 }
 
-// Create an instance of DBClient
 const dbClient = new DBClient();
 
-// Export DBClient as default export.
-module.exports = dbClient;
+export default dbClient;
--- utils/redis.js
+++ utils/redis.js
@@ -1,45 +1,32 @@
-// Import the redis and util modules
-const { createClient } = require('redis');
-const { promisify } = require('util');
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    // Create a Redis client
     this.myClient = createClient();
-
-    // Set up an error handler
     this.myClient.on('error', (error) => console.log(error));
   }
 
-  // Check if Redis client is connected to the server
   isAlive() {
     return this.myClient.connected;
   }
 
-  // Get the value associated with the given key from Redis
   async get(key) {
-    // Convert the `GET` command to a promise-based function using `promisify`
     const getAsync = promisify(this.myClient.GET).bind(this.myClient);
     return getAsync(key);
   }
 
-  // Set the value associated with the given key in Redis
   async set(key, val, time) {
-    // Convert the `SET` command to a promise-based function using `promisify`
     const setAsync = promisify(this.myClient.SET).bind(this.myClient);
     return setAsync(key, val, 'EX', time);
   }
 
-  // Delete the key-value pair associated with the given key from Redis
   async del(key) {
-    // Convert the `DEL` command to a promise-based function using `promisify`
     const delAsync = promisify(this.myClient.DEL).bind(this.myClient);
     return delAsync(key);
   }
 }
 
-// Create a new Redis client instance
 const redisClient = new RedisClient();
 
-// Export the Redis client instance
-module.exports = redisClient;
+export default redisClient;
