1d9c14f8f3341fcb8b9d82378a5bfe48930ff5ba
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-28 23:00:09 +0100

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,37 +1,67 @@
-const UtilController = require('./UtilController');
+// Importing required modules
+const sha1 = require('sha1');
+const { ObjectID } = require('mongodb');
+const Queue = require('bull');
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+// Creating a new queue instance
+const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
 class UsersController {
-  // Creates a new user with the provided email and password.
-  static async postNew(request, response) {
+  // Handles the creation of a new user
+  static postNew(request, response) {
     const { email, password } = request.body;
 
-    if (!email || !password) {
-      // Missing email or password
-      response.status(400).json({ error: `Missing ${!email ? 'email' : 'password'}` }).end();
-    } else if (await dbClient.userExists(email)) {
-      // User already exists
-      response.status(400).json({ error: 'Already exist' }).end();
-    } else {
-      try {
-        const passwordHash = UtilController.SHA1(password);
-        const insert = await dbClient.newUser(email, passwordHash);
-        const { _id } = insert.ops[0];
-        const _email = insert.ops[0].email;
-        response.status(201).json({ id: _id, email: _email }).end();
-      } catch (err) {
-        response.status(400).json({ error: err.message }).end();
-      }
+    // Checking for missing email and password
+    if (!email) {
+      response.status(400).json({ error: 'Missing email' });
+      return;
     }
+    if (!password) {
+      response.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const users = dbClient.db.collection('users');
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users
+          .insertOne({
+            email,
+            password: hashedPassword,
+          })
+          .then((result) => {
+            response.status(201).json({ id: result.insertedId, email });
+            userQueue.add({ userId: result.insertedId });
+          })
+          .catch((error) => console.log(error));
+      }
+    });
   }
 
-  // Retrieves the user information for the authenticated user.
+  // Retrieves the details of the authenticated user
   static async getMe(request, response) {
-    const { usr } = request;
-    delete usr.password;
-    usr.id = usr._id;
-    delete usr._id;
-    response.status(200).json(usr).end();
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection('users');
+      const idObject = new ObjectID(userId);
+      users.findOne({ _id: idObject }, (err, user) => {
+        if (user) {
+          response.status(200).json({ id: userId, email: user.email });
+        } else {
+          response.status(401).json({ error: 'Unauthorized' });
+        }
+      });
+    } else {
+      console.log('Hupatikani!');
+      response.status(401).json({ error: 'Unauthorized' });
+    }
   }
 }
 
