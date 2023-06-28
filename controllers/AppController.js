707dfa23ac88066dd118289987eaf40c0463cdd6
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-28 20:31:05 +0100

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,9 +4,8 @@ const dbClient = require('../utils/db');
 class AppController {
   // Get the status of Redis and the database
   static async getStatus(request, response) {
-    const redisStatus = redisClient.isAlive();
-    const dbStatus = dbClient.isAlive();
-    response.set('Content-Type', 'application/json');
+    const redisStatus = await redisClient.isAlive();
+    const dbStatus = await dbClient.isAlive();
     response.status(200).json({ redis: redisStatus, db: dbStatus }).end();
   }
 
@@ -14,7 +13,6 @@ class AppController {
   static async getStats(request, response) {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
-    response.set('Content-Type', 'application/json');
     response.status(200).json({ users, files }).end();
   }
 }
